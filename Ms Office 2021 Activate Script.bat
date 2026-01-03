caaa4c0f28aadeddc59f8fea0cb89c57c68a1ada
Smart HN <125441761+HN2MODE@users.noreply.github.com>
2026-01-03 22:09:34 +0530

    Add files via upload

---
+++ Ms Office 2021 Activate Script.bat
@@ -0,0 +1,18 @@
+@echo off
+title (HN_System)-Activate Microsoft Office 2021 (ALL versions) for FREE - office.com&cls&echo =====================================================================================&echo #Project: (HN_System)-Activating Microsoft software products for FREE without additional software&echo =====================================================================================&echo.&echo #Supported products:&echo - Microsoft Office Standard 2021&echo - Microsoft Office Professional Plus 2021 (Press Enter to Continue The Activation) &echo.&echo.&(if exist "%ProgramFiles%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles%\Microsoft Office\Office16")&(if exist "%ProgramFiles(x86)%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles(x86)%\Microsoft Office\Office16")&(for /f %%x in ('dir /b ..\root\Licenses16\ProPlus2021VL_KMS*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul)&echo.&echo =====================================================================================&echo Activating your product...&cscript //nologo slmgr.vbs /ckms >nul&cscript //nologo ospp.vbs /setprt:1688 >nul&cscript //nologo ospp.vbs /unpkey:6F7TH >nul&set i=1&cscript //nologo ospp.vbs /inpkey:FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH >nul||goto notsupported
+:skms
+if %i% GTR 10 goto busy
+if %i% EQU 1 set KMS=kms7.office.com
+if %i% EQU 2 set KMS=e8.us.to
+if %i% EQU 3 set KMS=e9.us.to
+if %i% GTR 3 goto ato
+cscript //nologo ospp.vbs /sethst:%KMS% >nul
+:ato
+echo =====================================================================================&echo.&echo.&cscript //nologo ospp.vbs /act | find /i "successful" && (echo.&echo =====================================================================================&echo.&echo #Please Support me but can contact me.Follow Me.Thankz for using HN_System...!&echo.&echo =====================================================================================&choice /n /c YN /m "Would you like to Contact Me [Y,N]?" & if errorlevel 2 exit) || (echo The connection to my KMS server failed! Trying to connect to another one... & echo Please wait... & echo. & echo. & set /a i+=1 & goto skms)
+explorer "https://wa.me/0717749974?text=Hii%20Smart2HN!%20%3C%3E.%20"&goto halt
+:notsupported
+echo =====================================================================================&echo.&echo Sorry, your version is not supported.&echo.&goto halt
+:busy
+echo =====================================================================================&echo.&echo Sorry, the server is busy and can't respond to your request. Please try again.&echo.
+:halt
+pause >nul
